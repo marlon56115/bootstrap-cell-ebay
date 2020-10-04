39d9402cb39ec5cfbfcc5a87d10afcee183e1190
marlon <marlon56115@gmail.com>
2020-10-03 18:50:55 -0600

    Se cambiaron los colores de botones y se modifico el formulario de busqueda por precio y se cambio el archivo de charts a charts.min porque fallaba

--- export/assets/js/scriptsGraficas.js
+++ export/assets/js/scriptsGraficas.js
@@ -1,3 +1,4 @@
+//primer grafica
 var ctx1 = document.getElementById("MasVendidosDia").getContext('2d');
 var MasVendidosDia = new Chart(ctx1, {
     type: 'bar',
@@ -49,7 +50,7 @@ var MasVendidosDia = new Chart(ctx1, {
     }
 });
 
-
+//segunda grafica
 var datapoints = [0, 20, 20, 60, 60, 120];
 var config = {
     type: 'line',
@@ -99,11 +100,8 @@ var ctx2 = document.getElementById("masCaros").getContext('2d');
 var masCaros = new Chart(ctx2, config)
 
 
-
-var mejorPuntuados = document.getElementById("mejorPuntuados");
-//Chart.defaults.global.defaultFontFamily = "Lato";
-//Chart.defaults.global.defaultFontSize = 18;
-
+//tercer grafica
+var ctx3 = document.getElementById("mejorPuntuados").getContext('2d');
 var oilData = {
     labels: [
         "Samsung",
@@ -124,34 +122,28 @@ var oilData = {
         }
 };
 
-var pieChart = new Chart(mejorPuntuados, {
+var pieChart = new Chart(ctx3, {
   type: 'pie',
   data: oilData
 });
 
 
 
-
-var ctx3 = document.getElementById("categoriasDestacadas").getContext('2d');
-var MasVendidosDia = new Chart(ctx3, {
+//cuarta grafica
+var ctx4 = document.getElementById("categoriasDestacadas").getContext('2d');
+var MasVendidosDia = new Chart(ctx4, {
     type: 'bar',
     data: {
-        labels: ["J6", "Iphone 12", "Note 10", "One Plus 6", "Redmi note 9", "Rog phone"],
+        labels: [ "Smartphones", "Tablets", "Gadgets"],
         datasets: [{
             label: 'Ocultar',
             data: [12, 19, 3, 5, 2, 3],
             backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
                 'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
                 'rgba(153, 102, 255, 0.2)',
                 'rgba(255, 159, 64, 0.2)'
             ],
             borderColor: [
-                'rgba(255,99,132,1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
                 'rgba(75, 192, 192, 1)',
                 'rgba(153, 102, 255, 1)',
                 'rgba(255, 159, 64, 1)'
@@ -166,7 +158,7 @@ var MasVendidosDia = new Chart(ctx3, {
                 display: true,
                 scaleLabel: {
                     display: true,
-                    labelString: 'Modelo'
+                    labelString: 'Categoria'
                 }
             }],
             yAxes: [{
