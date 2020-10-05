10132f8a4424aedd062c7af612c248f7e809b4eb
marlon <marlon56115@gmail.com>
2020-10-04 23:30:09 -0600

    se añadio un proyecto node para cambiar css de bootstrap, se movi el boton de fecha y se añadieron toasta para dar las notificaciones

---
+++ export/assets/js/toasts.js
@@ -0,0 +1,15 @@
+function alertaCarritoAdd() {
+    $('#toastCArrito').collapse('show');
+    $('#toastCArrito').toast('show');
+    
+    /**setTimeout(function () {
+        $('#toastCArrito').toast('hide');
+        $('#toastCArrito').collapse('hide');
+    }, 1000)*/
+
+}
+
+function alertaAnuncioAdd() {
+    $('#toastAnuncio').collapse('show');
+    $('#toastAnuncio').toast('show');
+}
